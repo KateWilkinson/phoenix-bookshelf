641abfa1a37ec1d4705dca158e88a0381ef87f72
Kate Wilkinson <kate.wilkinson@hotmail.com>
2016-03-25 13:55:11 +0000

    redirecting / to /books

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,6 +2,6 @@ defmodule PhoenixBookshelf.PageController do
   use PhoenixBookshelf.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    redirect conn, to: "/books"
   end
 end
